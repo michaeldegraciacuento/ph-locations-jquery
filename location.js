896a97b2648f08b8a67758913fcff0b812645467
Mac Story <amfufu.michael@gmail.com>
2022-11-24 11:27:37 +0800

    Update location.js

--- location.js
+++ location.js
@@ -1,8 +1,7 @@
 
          let selectedRegion = $('#region').children("option:selected").val();
          let selectedCities = $('#cities').children("option:selected").val();
-
-         
+ 
         var region_fetch = $.getJSON( "https://psgc.gitlab.io/api/regions/", function(data) {
 
             //console.log(region_fetch);
@@ -18,10 +17,13 @@
     
         $(function(){
             $("#region").on('change',function(){
-                $('#province').empty();$('#cities').empty();$('#barangays').empty();
+                $('#province').empty();
+                $('#cities').empty();
+                $('#barangays').empty();
+                $('#province').val( $('#province').find("option[selected]").val() );
                 var selectedRegion = $(this).children("option:selected").val();
                 var fill_provinces = "https://psgc.gitlab.io/api/regions/" + selectedRegion + "/provinces";
-                //console.log(fill_provinces);
+                console.log(fill_provinces);
 
                  var province_fetch = $.getJSON(fill_provinces, function(data){
                         var html = '';
@@ -31,10 +33,13 @@
                             html += '<option value="' + data[i].code + '">' +    data[i].name + '</option>';
                         }
                         $('#province').append(html);
+                        
                  });
+                 
                 });
             $("#province").on('change',function(){
-                $('#cities').empty();$('#barangays').empty();
+                $('#cities').empty();
+                $('#barangays').empty();
                 var selectedProvince = $(this).children("option:selected").val();
                 var fill_cities = "https://psgc.gitlab.io/api/provinces/" + selectedProvince + '/cities-municipalities/';
                 //console.log(fill_cities);
